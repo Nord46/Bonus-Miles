3bab4db1e0dd4ddaa3b749fb32bdbd0577e4d4ef
Nord46 <stanislav.kulakov0304@gmail.com>
2022-06-24 21:08:48 +0300

    Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticket = 4500;
+        int bonus = 20;
+        int bonusMiles = ticket / bonus;
+        System.out.println("Итоговый бонус: " + bonusMiles);
+    }
+}
